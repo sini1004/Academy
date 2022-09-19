dd7bbadbbddc7cea761561591fe000850bc872be
sini <tlsgml656@gmail.com>
2022-09-19 14:38:33 +0900

    date.220919-update

--- _01.javascript/05_function/04_memory.js
+++ _01.javascript/05_function/04_memory.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /**
 함수는 heap에 들어감.
 
@@ -15,22 +15,4 @@ const sum = add;
 console.log('add함수 실행 : ', add(2, 3));
 console.log('sum함수 실행 : ', sum(3, 7));
 console.log('add함수 실행 : ', add());
-=======
-/**
-함수는 heap에 들어감.
-
- */
-
-function add(a, b){
-  return a + b;
-}
-
-const sum = add;
-//add가 가르키는 메모리 주소
-//sum도 함수처럼 사용
-
-console.log('add함수 실행 : ', add(2, 3));
-console.log('sum함수 실행 : ', sum(3, 7));
-console.log('add함수 실행 : ', add());
->>>>>>> 92db50fe19709d7b82f5517ffacd8416648d1c89
 console.log('sum함수 실행 : ', sum());
--- _01.javascript/05_function/05_return.js
+++ _01.javascript/05_function/05_return.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /**
 return을 명시하지 않으면 undefined반환
  */
@@ -27,34 +27,5 @@ function print(num){
   }
   console.log(num)
 }
-=======
-/**
-return을 명시하지 않으면 undefined반환
- */
 
-function add(a, b){
-  a + b;
-  
-}
-//return을 명시하지 않으면 undefined반환
-
-const result = add(3, 5);
-
-console.log('add함수 실행', result);
-
-
-function write(tt){
-  console.log('맛있는 새우튀김🍤', tt);
-}
-//특정한 일 수행하거나 반환하지 않는 함수는 return 생략 가능
-write('good')
-
-function print(num){
-  if(num < 0){
-    return; //undefined
-    //조건에 맞으면 리턴으로 함수를 종료, return undefined의 축약
-  }
-  console.log(num)
-}
->>>>>>> 92db50fe19709d7b82f5517ffacd8416648d1c89
 print(12);
