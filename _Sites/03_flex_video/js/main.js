0554944add004c3b5047c87e5c897308cda1466a
shinhee <tlsgml656@gmail.com>
2022-11-14 16:34:20 +0900

    article클릭 시 상세하게 페이지 보여주기

--- _Sites/03_flex_video/js/main.js
+++ _Sites/03_flex_video/js/main.js
@@ -1,5 +1,9 @@
-//
+//article hover viedo play
 const items = document.querySelectorAll("article");
+
+//aside
+const aside = document.querySelector("aside");
+const close = document.querySelector("aside span");
 
 for (let el of items) {
 	el.addEventListener("mouseenter", (e) => {
@@ -9,4 +13,22 @@ for (let el of items) {
 	el.addEventListener("mouseleave", (e) => {
 		e.currentTarget.querySelector("video").pause();
 	});
+
+	el.addEventListener("click", (e) => {
+		let title = e.currentTarget.querySelector("h2").innerText;
+		let txt = e.currentTarget.querySelector("p").innerText;
+		let video = e.currentTarget.querySelector("video").getAttribute("src");
+
+		aside.querySelector("h2").innerText = title;
+		aside.querySelector("p").innerText = txt;
+		aside.querySelector("video").setAttribute("src", video);
+		aside.querySelector("video").play();
+
+		aside.classList.add("on");
+	});
 }
+
+close.addEventListener("click", () => {
+	aside.classList.remove("on");
+	aside.querySelector("video").pause();
+});
