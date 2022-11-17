c29e584627d176610162d1733f16f2dca4966ed9
shinhee <tlsgml656@gmail.com>
2022-11-17 10:54:23 +0900

    promise 문제점 해결_async/await

--- _01.javascript/15_study/02_promise.js
+++ _01.javascript/15_study/02_promise.js
@@ -18,6 +18,16 @@ function increaseAndPrint(n){
 }
 
 increaseAndPrint(0)
-  .then((n) => {
-    console.log('value : ', n)
-  })
+  .then(increaseAndPrint)
+  .then(increaseAndPrint)
+  .then(increaseAndPrint)
+  .then(increaseAndPrint)
+  .catch((e) => console.error(e))
+
+  /**
+   * 아쉬운 점 : 몇번째에서 에러 발생한지 모름.
+   * 특정 조건에 따라 분기를 나누는 작업도 어려움.
+   * 특정 값을 공유해가면서 작업 처리하기도 힘듬.
+   * then이 너무 중복됨.
+   * => async/await 
+   */
